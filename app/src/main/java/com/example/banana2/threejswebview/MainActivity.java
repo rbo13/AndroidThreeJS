93dc2bfb15af5e5a7baba37f4c3eff5bc6af5772
banana2 <banana2@banana2s-MacBook-Pro.local>
2017-02-28 10:28:34 +0800

    Initial commit with working 3D Render

---
+++ app/src/main/java/com/example/banana2/threejswebview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.banana2.threejswebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.xwalk.core.XWalkView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private XWalkView mXWalkView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mXWalkView = (XWalkView) findViewById(R.id.xWalkView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mXWalkView.load("https://tympanus.net/Tutorials/TheAviator/", null);
+    }
+}
